fca257d067cf578f3323839f03f7828ea53adfa1
JonasWitzenrath <jonas.witzenrath@fishpost.de>
2018-05-30 01:26:07 +0200

    Arduino-Test-Programm zur PWM-Generation hinzugefügt

---
+++ Programme/Arduino-Programme/arduino_pca_emulator/arduino_pca_emulator/arduino_pca_emulator.ino
@@ -0,0 +1,61 @@
+//default values
+int freq = 100;
+int pwm = 50;
+
+int f_corr = 1;
+
+void serialEvent(){
+	delay(100);
+	int n = Serial.available();
+	char in_buf[n-1];
+
+	char com = Serial.read();
+	for (int i = 0; i < n-1; i++){
+		in_buf[i] = Serial.read();
+	}
+	int arg = atoi(in_buf);
+
+	switch (com){
+		case 'f':
+			//set frequency in kHz
+			(arg > 62) ? freq = arg : freq = 63;
+			break;
+		case 'd':
+			pwm = arg;
+			break;
+	}
+	int compA = 16000 / freq - f_corr;
+	int compB = compA * float(pwm)/100.;
+	noInterrupts();
+	OCR2A = compA;
+	OCR2B = compB;
+	interrupts();
+	
+	Serial.print("f = ");
+	Serial.print(freq);
+	Serial.print("kHz @ ");
+	Serial.print(pwm);
+	Serial.println(" % Duty");
+}
+
+void setup() {
+	Serial.begin(19200);
+	DDRB = 0xff;
+	DDRD = 0xff;
+
+	int compA = 16000 / freq - f_corr;
+	int compB = compA * float(pwm)/100.;
+	
+	noInterrupts();
+	TCCR2A = _BV(WGM21) | _BV(WGM20) | _BV(COM2B1);
+	TCCR2B = _BV(CS20) | _BV(WGM22);
+	TCNT2  = 0x00;
+
+	OCR2A = compA;
+	OCR2B = compB;
+	interrupts();
+}
+
+void loop() {
+	//nop
+}
