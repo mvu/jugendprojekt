f9561b095b59c1c9a6ef55ac754a1726abcb0e34
jwitzenrath <jonas.witzenrath@fishpost.de>
2019-01-17 20:25:48 +0100

    Bug entdeckt und kommentiert

--- Programme/ControlPanel-3-0/inc/model/application.h
+++ Programme/ControlPanel-3-0/inc/model/application.h
@@ -8,16 +8,21 @@
 
 /*!
  * \brief Anpassung von QApplication an das Touchpad
- */
+  */
 class Application final : public QApplication
 {
 public:
     Application(int& argc, char** argv);
-    /*! \brief Blockt zu dicht aufeinanderfolgende Mausklicks
-     *  \details Die Zeit, die mindestens zwischen den Events liegen muss,
+    /*!
+     * \brief Blockt zu dicht aufeinanderfolgende Mausklicks
+     * \details Die Zeit, die mindestens zwischen den Events liegen muss,
      *      wird über min_time_passed_ms_ festgelegt. Der Standardwert ist 100 ms.
+     * \bug Fängt nur MouseButtonRelease Events ab, d.h. MouseButtonPress events
+     *      werden immer durch gereicht. Dadurch verbleiben die Buttons bei zu
+     *      schnellem Doppelklick im Zustand pressed (sichtbar duch dauerhaft
+     *      verringerte Transparenz).
      */
-    bool notify(QObject *receiver /*!< Der Empfänger des Signals */, QEvent *e /*!< Das Event */);
+    bool notify(QObject *receiver, QEvent *e);
 
 private:
     qint64 timestamp_;
